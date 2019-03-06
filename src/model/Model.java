69fc5d3a8fc58389f6d723c1ee41a205068b008a
ibogf <79500420830@yandex.ru>
2019-03-06 15:02:37 +0300

    first commit \ first version of model

---
+++ src/model/Model.java
@@ -0,0 +1,38 @@
+package model;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * in this class i save model of my application
+ */
+
+public class Model
+{
+
+    private HashMap<Float,String> arrOfTaskWhizNoFine;
+    private HashMap<Float,String> arrOfTaskWhizFine;
+
+   void clear()
+   {
+      arrOfTaskWhizFine.clear();
+      arrOfTaskWhizNoFine.clear();
+   }
+
+    public HashMap<Float, String> getArrOfTaskWhizFine() {
+        return arrOfTaskWhizFine;
+    }
+
+    public void setArrOfTaskWhizFine(HashMap<Float, String> arrOfTaskWhizFine) {
+        this.arrOfTaskWhizFine = arrOfTaskWhizFine;
+    }
+
+    public HashMap<Float, String> getArrOfTaskWhizNoFine() {
+        return arrOfTaskWhizNoFine;
+    }
+
+    public void setArrOfTaskWhizNoFine(HashMap<Float, String> arrOfTaskWhizNoFine) {
+        this.arrOfTaskWhizNoFine = arrOfTaskWhizNoFine;
+    }
+}
